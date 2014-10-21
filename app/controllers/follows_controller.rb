c0653f1f6742ad77eade0b3b3a8d1769b37c905f
Elijah <kimelijah90@gmail.com>
2014-10-20 20:53:26 -0400

    Added unfollow button
    *unfollow button will show only when folling a user
    *follow button will show when not following a user
    *added "user_path" and got it to work as intended

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,6 +2,12 @@ class FollowsController < ApplicationController
   def create
     user = User.find(params[:id])
     user.followers << current_user
-    redirect_to "/#{current_user.username}"
+    redirect_to user_path(user.username)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.followers.delete(current_user)
+    redirect_to user_path(user.username)
   end
 end
