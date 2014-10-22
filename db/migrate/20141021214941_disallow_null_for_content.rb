f280753ad2cd8b3cbf71f856750080ed2e296be3
kaffin <katherin.siracusa@gmail.com>
2014-10-22 18:29:29 -0400

    added search functionality

--- db/migrate/20141021214941_disallow_null_for_content.rb
+++ db/migrate/20141021214941_disallow_null_for_content.rb
@@ -1,6 +1,6 @@
 class DisallowNullForContent < ActiveRecord::Migration
   def change
     change_column :shouts, :content_type, :string, null: false
-    change_column :shouts, :content_id, :string, null: false
+    change_column :shouts, :content_id, :integer, null: false
   end
 end
