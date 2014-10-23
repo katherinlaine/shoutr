e1b7a2e9688c25f600a83ab21f6cb3aa617527b6
kaffin <katherin.siracusa@gmail.com>
2014-10-23 17:17:12 -0400

    Added links to #hashtags
    * link to search shouts for #hashtag text

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -4,4 +4,10 @@ module ShoutsHelper
       link_to username, user_path(username.gsub('@',''))
     end.html_safe
   end
+
+  def link_hashtags(text)
+    text.gsub(/#\w+/) do |hashtag|
+      link_to hashtag, search_path(query: hashtag)
+    end.html_safe
+  end
 end
